6ddd8476498ad36dc1eb201bd4143470e3ce4888
aithasahith02 <aithasahith0214@gmail.com>
2025-04-25 19:13:05 -0400

    Added metrics endpoint for Prometheus

--- rl-agent/main.py
+++ rl-agent/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from rl_env import DummyEnv
+from prometheus_client import generate_latest
+from fastapi.responses import PlainTextResponse
 
 app = FastAPI()
 
@@ -9,6 +11,7 @@ env = DummyEnv()   # Instance of Dummy ENV
 def root():
     return {"message": "IntelliScale RL Agent is Running!"}
 
+# Decision API
 @app.post("/decide/")
 def decide(cpu: float, memory: float, request_rate: float):
     """
@@ -18,3 +21,7 @@ def decide(cpu: float, memory: float, request_rate: float):
     action = env.decide_action(state)   
     return {"action": action}
 
+# Metrics Endpoint for Prometheus
+@app.get("/metrics")
+def metrics():
+    return PlainTextResponse(generate_latest())
