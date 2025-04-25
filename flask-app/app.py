54e9ae0991342d1778ecd479e49ddf4cf3c9bb5a
aithasahith02 <aithasahith0214@gmail.com>
2025-04-25 19:10:50 -0400

    Added /metrics endpoint for Prometheus

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,14 +1,22 @@
 # This flask app is developed to simlulates load
+from prometheus_client import generate_latest, CONTENT_TYPE_LATEST
 from flask import Flask
+
 import random
 import time
 
 app = Flask(__name__)
 
+# Regular endpoint
 @app.route("/")
 def index():
     time.sleep(random.uniform(0.05, 0.5)) 
     return "This is Sample Flask app by Sahith Aitha!"
+
+# Endpoint to expose metrics to prometheus
+@app.route("/metrics")
+def metrics():
+    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
